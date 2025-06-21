f3eb2733ff57f3025d44aee9d056bd9950e36df6
butiyam <butiyam@gmail.com>
2025-06-21 13:25:32 +0530

    sidebar menu

--- src/app/leaderboard/page.tsx
+++ src/app/leaderboard/page.tsx
@@ -1,10 +1,8 @@
 "use client";
-import Navbar from "../Navbar";
 import Leaderboard from "../../../components/Leaderboard";
 
 export default function leaderboard() {
   return  <> 
-          <Navbar />
           <Leaderboard />
         </>;
 }
