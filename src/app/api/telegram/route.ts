37874823aa30024ea1daf820f260ca355ee31331
butiyam <butiyam@gmail.com>
2025-06-26 12:37:56 +0530

    responsive

--- src/app/api/telegram/route.ts
+++ src/app/api/telegram/route.ts
@@ -8,21 +8,36 @@ const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN!);
 bot.start(async (ctx) => {
   
   const userId = ctx.from.id;
-  const username = ctx.from.username || '';
+  const username = ctx.from.username || '';  
+  const member = await ctx.telegram.getChatMember(process.env.TELEGRAM_GROUP_ID!, userId);
 
-   try {
-    await db.query(
-      `INSERT INTO users (user_id, username)
-       VALUES (?, ?)
-       ON DUPLICATE KEY UPDATE username = ?`,
-      [userId, username, username]
-    );
+if (
+    member.status === 'member' ||
+    member.status === 'creator' ||
+    member.status === 'administrator'
+  ) {
+   // ctx.reply('✅ You have already joined the group. Thanks!');
+      try {
+            await db.query(
+            `INSERT INTO users (user_id, username, telegram_joined)
+            VALUES (?, ?, ?)
+            ON DUPLICATE KEY UPDATE username = ?`,
+            [userId, username, 1,username]
+            );
 
-    ctx.reply(`👋 Hello ${username}! You've been registered successfully.`);
-  } catch (err) {
-    console.error('DB Insert Error:', err);
-    ctx.reply('⚠️ Failed to save your details. Please try again later.');
+            ctx.reply(`👋 Hello ${username}! You've been registered successfully.`);
+
+
+        } catch (err) {
+            console.error('DB Insert Error:', err);
+            ctx.reply('⚠️ Failed to save your details. Please try again later.');
+        }
+        
+  } else {
+    ctx.reply('❌ Please join our Telegram group before proceeding: https://t.me/dyfusion');
   }
+
+  
 });
 
 bot.launch();
